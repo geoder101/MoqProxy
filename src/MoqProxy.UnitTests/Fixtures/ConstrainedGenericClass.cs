bbdc31e7a692f8445b699bdb5478b55456bb86b2
George Dernikos <geoder101@gmail.com>
2025-11-02 17:12:47 +0200

    add missing license headers

--- src/MoqProxy.UnitTests/Fixtures/ConstrainedGenericClass.cs
+++ src/MoqProxy.UnitTests/Fixtures/ConstrainedGenericClass.cs
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 George Dernikos <geoder101@gmail.com>
+
 namespace MoqProxy.UnitTests.Fixtures;
 
 public class ConstrainedGenericClass<T> where T : struct;
